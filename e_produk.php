781c42d55e789f010a6c158cda78e59de5c5c45b
25550005 <indrayudhaaxel@gmail.com>
2026-05-13 09:30:26 +0700

    update project php

--- e_produk.php
+++ e_produk.php
@@ -96,7 +96,7 @@ if (isset($_POST['update'])) {
     <header id="header" class="header fixed-top d-flex align-items-center">
 
         <div class="d-flex align-items-center justify-content-between">
-            <a href="index.html" class="logo d-flex align-items-center">
+            <a href="index.php" class="logo d-flex align-items-center">
                 <img src="assets/img/logo.png" alt="">
                 <span class="d-none d-lg-block">Street Mile</span>
             </a>
